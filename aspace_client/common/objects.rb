6398327ad95f96b8bee88f228641552494d140b6
Jacob Shelby <jacobzaccount@gmail.com>
2022-07-22 12:40:36 -0400

    DRY'd up attach methods and refactored to accept data rather than filepaths

--- aspace_client/common/objects.rb
+++ aspace_client/common/objects.rb
@@ -7,80 +7,116 @@ module Common
       end
     end
 
-    desc 'attach_classifications PATH, FILE, FIELD', 'attach classification refs to object by matching values from the given field. assumes FIELD is an array'
-    def attach_classifications(path,file,field)
-      index = invoke 'common:classifications:make_index'
-      data = JSON.parse(File.read(File.join(path,file)))
-      data.each do |record|
-        classification_refs = []
-        record[field].each do |classification|
-          classification_refs << index[classification]
+    # setting up DRY attach methods for classifications and subjects
+    %w(classifications subjects).each do |entity_type|
+      desc "attach_#{entity_type}", "attach #{entity_type} refs to object by matching values from the given field. assumes DATA is an array of hashes, FIELD is a string"
+      define_method("attach_#{entity_type}") do |data,field|
+        index = invoke "common:#{entity_type}:make_index"
+        data.each do |record|
+          variable_name = "@#{entity_type}_refs"
+          instance_variable_set(variable_name,[])
+          record[field].each do |entity|
+            instance_variable_get(variable_name) << index[entity]
+          end
+          record["#{entity_type}__refs"] = instance_variable_get(variable_name)
         end
-        record['classification__refs'] = classification_refs
-      end
       
-      data
+        data
+      end
     end
 
-    desc 'attach_subjects PATH, FILE, FIELD', 'attach subject refs to object by matching values from the given field. assumes FIELD is an array'
-    def attach_subjects(path,file,field)
-      index = invoke 'common:subjects:make_index'
-      data = JSON.parse(File.read(File.join(path,file)))
-      data.each do |record|
-        subject_refs = []
-        record[field].each do |subject|
-          subject_refs << index[subject]
+      # setting up DRY attach methods for agents
+    %w(corporate_entities families people).each do |entity_type|
+      desc "attach_#{entity_type}", "attach #{entity_type} refs to object by matching values from the given field. assumes DATA is an array of hashes, FIELD and ROLE are strings"
+      define_method("attach_#{entity_type}") do |data,field,role|
+        index = invoke "common:#{entity_type}:make_index"
+        data.each do |record|
+          variable_name = "@#{entity_type}_refs"
+          instance_variable_set(variable_name,[])
+          record[field].each do |entity|
+            instance_variable_get(variable_name) << {'ref' => index[entity], 'role' => role}
+          end
+          record["#{entity_type}__refs"] = instance_variable_get(variable_name)
         end
-        record['subject__refs'] = subject_refs
-      end
       
-      data
+        data
+      end
     end
 
-    desc 'attach_linked_people PATH, FILE, FIELD, ROLE', 'attach linked people refs to object by matching values from the given field. assume FIELD is an array and ROLE is a string'
-    def attach_linked_people(path,file,field,role)
-      index = invoke 'common:agents:make_index_people'
-      data = JSON.parse(File.read(File.join(path,file)))
-      data.each do |record|
-        linked_people_refs = []
-        record[field].each do |person|
-          linked_people_refs << {'ref' => index[person], 'role' => role}
-        end
-        record['linked_people__refs'] = linked_people_refs
-      end
+    # desc 'attach_classifications PATH, FILE, FIELD', 'attach classification refs to object by matching values from the given field. assumes FIELD is an array'
+    # def attach_classifications(path,file,field)
+    #   index = invoke 'common:classifications:make_index'
+    #   data = JSON.parse(File.read(File.join(path,file)))
+    #   data.each do |record|
+    #     classification_refs = []
+    #     record[field].each do |classification|
+    #       classification_refs << index[classification]
+    #     end
+    #     record['classification__refs'] = classification_refs
+    #   end
       
-      data
-    end
+    #   data
+    # end
 
-    desc 'attach_linked_corporate PATH, FILE, FIELD, ROLE', 'attach linked corporate refs to object by matching values from the given field. assume FIELD is an array and ROLE is a string'
-    def attach_linked_corporate(path,file,field,role)
-      index = invoke 'common:agents:make_index_corporate'
-      data = JSON.parse(File.read(File.join(path,file)))
-      data.each do |record|
-        linked_corporate_refs = []
-        record[field].each do |corporate|
-          linked_corporate_refs << {'ref' => index[corporate], 'role' => role}
-        end
-        record['linked_corporate__refs'] = linked_corporate_refs
-      end
+    # desc 'attach_subjects PATH, FILE, FIELD', 'attach subject refs to object by matching values from the given field. assumes FIELD is an array'
+    # def attach_subjects(path,file,field)
+    #   index = invoke 'common:subjects:make_index'
+    #   data = JSON.parse(File.read(File.join(path,file)))
+    #   data.each do |record|
+    #     subject_refs = []
+    #     record[field].each do |subject|
+    #       subject_refs << index[subject]
+    #     end
+    #     record['subject__refs'] = subject_refs
+    #   end
       
-      data
-    end
+    #   data
+    # end
 
-    desc 'attach_linked_families PATH, FILE, FIELD, ROLE', 'attach linked family refs to object by matching values from the given field. assume FIELD is an array and ROLE is a string'
-    def attach_linked_families(path,file,field,role)
-      index = invoke 'common:agents:make_index_families'
-      data = JSON.parse(File.read(File.join(path,file)))
-      data.each do |record|
-        linked_families_refs = []
-        record[field].each do |family|
-          linked_families_refs << {'ref' => index[family], 'role' => role}
-        end
-        record['linked_families__refs'] = linked_families_refs
-      end
+    # desc 'attach_linked_people PATH, FILE, FIELD, ROLE', 'attach linked people refs to object by matching values from the given field. assume FIELD is an array and ROLE is a string'
+    # def attach_linked_people(path,file,field,role)
+    #   index = invoke 'common:agents:make_index_people'
+    #   data = JSON.parse(File.read(File.join(path,file)))
+    #   data.each do |record|
+    #     linked_people_refs = []
+    #     record[field].each do |person|
+    #       linked_people_refs << {'ref' => index[person], 'role' => role}
+    #     end
+    #     record['linked_people__refs'] = linked_people_refs
+    #   end
       
-      data
-    end
+    #   data
+    # end
+
+    # desc 'attach_linked_corporate PATH, FILE, FIELD, ROLE', 'attach linked corporate refs to object by matching values from the given field. assume FIELD is an array and ROLE is a string'
+    # def attach_linked_corporate(path,file,field,role)
+    #   index = invoke 'common:agents:make_index_corporate'
+    #   data = JSON.parse(File.read(File.join(path,file)))
+    #   data.each do |record|
+    #     linked_corporate_refs = []
+    #     record[field].each do |corporate|
+    #       linked_corporate_refs << {'ref' => index[corporate], 'role' => role}
+    #     end
+    #     record['linked_corporate__refs'] = linked_corporate_refs
+    #   end
+      
+    #   data
+    # end
+
+    # desc 'attach_linked_families PATH, FILE, FIELD, ROLE', 'attach linked family refs to object by matching values from the given field. assume FIELD is an array and ROLE is a string'
+    # def attach_linked_families(path,file,field,role)
+    #   index = invoke 'common:agents:make_index_families'
+    #   data = JSON.parse(File.read(File.join(path,file)))
+    #   data.each do |record|
+    #     linked_families_refs = []
+    #     record[field].each do |family|
+    #       linked_families_refs << {'ref' => index[family], 'role' => role}
+    #     end
+    #     record['linked_families__refs'] = linked_families_refs
+    #   end
+      
+    #   data
+    # end
 
     desc 'get_resources', 'retrieve API response of all resource data in ASpace'
     def get_resources(*args)
