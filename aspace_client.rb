5b8fbaf3a6ce9c8f7608531d184961ce549de17d
Jacob Shelby <jacobzaccount@gmail.com>
2023-10-05 16:29:58 -0400

    Update aspace_client.rb
    Update base_uri to match Docker instance default.

--- aspace_client.rb
+++ aspace_client.rb
@@ -29,7 +29,7 @@ module Aspace_Client
 
   @@config = ArchivesSpace::Configuration.new({
     # set the base uri for your ArchivesSpace API instance
-    base_uri: 'http://localhost:8089',
+    base_uri: 'http://localhost:4567',
     username: ENV['ASPACE_USERNAME'],
     password: ENV['ASPACE_PASS'],
     page_size: 250,
